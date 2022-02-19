6b64ebfcb9dccb1f2530165bee3b5dde50436f1e
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-20 01:05:20 +0200

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.sheltonfragoso.springbootionicbackend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sheltonfragoso.springbootionicbackend.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
