d9177e1ba84349d80641702354058dc8efa4fa80
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-20 00:57:31 +0200

    Criando operacao de instanciacao

--- src/main/java/com/sheltonfragoso/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -1,13 +1,31 @@
 package com.sheltonfragoso.springbootionicbackend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sheltonfragoso.springbootionicbackend.domain.Categoria;
+import com.sheltonfragoso.springbootionicbackend.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringbootIonicBackendApplication {
+public class SpringbootIonicBackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
