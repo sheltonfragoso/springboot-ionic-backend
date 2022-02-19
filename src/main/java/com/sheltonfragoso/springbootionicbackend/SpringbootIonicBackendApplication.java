09c15319641a5ef98c9646b08423e277d81be6fa
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-20 01:43:01 +0200

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/sheltonfragoso/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sheltonfragoso.springbootionicbackend.domain.Categoria;
 import com.sheltonfragoso.springbootionicbackend.domain.Cidade;
+import com.sheltonfragoso.springbootionicbackend.domain.Cliente;
+import com.sheltonfragoso.springbootionicbackend.domain.Endereco;
 import com.sheltonfragoso.springbootionicbackend.domain.Estado;
 import com.sheltonfragoso.springbootionicbackend.domain.Produto;
+import com.sheltonfragoso.springbootionicbackend.domain.enums.TipoCliente;
 import com.sheltonfragoso.springbootionicbackend.repositories.CategoriaRepository;
 import com.sheltonfragoso.springbootionicbackend.repositories.CidadeRepository;
+import com.sheltonfragoso.springbootionicbackend.repositories.ClienteRepository;
+import com.sheltonfragoso.springbootionicbackend.repositories.EnderecoRepository;
 import com.sheltonfragoso.springbootionicbackend.repositories.EstadoRepository;
 import com.sheltonfragoso.springbootionicbackend.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class SpringbootIonicBackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
@@ -66,5 +77,17 @@ public class SpringbootIonicBackendApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
---
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.sheltonfragoso.springbootionicbackend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sheltonfragoso.springbootionicbackend.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
