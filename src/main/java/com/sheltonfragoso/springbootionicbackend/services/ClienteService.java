703b3976f99c8b4c68f0320bbce4b502c4017bd6
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-20 01:48:50 +0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.sheltonfragoso.springbootionicbackend.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sheltonfragoso.springbootionicbackend.domain.Cliente;
+import com.sheltonfragoso.springbootionicbackend.repositories.ClienteRepository;
+import com.sheltonfragoso.springbootionicbackend.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
