6255cfb1e41f0b83b367399fb843adcca2c2abe6
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-20 03:33:46 +0200

    Validacao sintatica com Bean Validation

--- src/main/java/com/sheltonfragoso/springbootionicbackend/services/CategoriaService.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sheltonfragoso.springbootionicbackend.domain.Categoria;
+import com.sheltonfragoso.springbootionicbackend.dto.CategoriaDTO;
 import com.sheltonfragoso.springbootionicbackend.repositories.CategoriaRepository;
 import com.sheltonfragoso.springbootionicbackend.services.exceptions.DataIntegrityException;
 import com.sheltonfragoso.springbootionicbackend.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
