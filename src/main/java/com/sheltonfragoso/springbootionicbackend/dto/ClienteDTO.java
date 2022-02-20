d9ff4f8e15e37bb42e8fc1bdc9ad7fbf56849e9b
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-20 04:19:43 +0200

    Validacao customizada: email nao repetido na actualizacao de Cliente

--- src/main/java/com/sheltonfragoso/springbootionicbackend/dto/ClienteDTO.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.sheltonfragoso.springbootionicbackend.domain.Cliente;
+import com.sheltonfragoso.springbootionicbackend.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
