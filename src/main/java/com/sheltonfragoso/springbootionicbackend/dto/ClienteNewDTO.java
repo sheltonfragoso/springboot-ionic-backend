b1736df2b248113664eed284f16bcff5c79e5f7a
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-21 20:38:02 +0200

    Adicionando senha a Cliente

--- src/main/java/com/sheltonfragoso/springbootionicbackend/dto/ClienteNewDTO.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/dto/ClienteNewDTO.java
@@ -23,6 +23,9 @@ public class ClienteNewDTO implements Serializable {
 	private String cpfOuCnpj;
 
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
@@ -151,5 +154,13 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 }
