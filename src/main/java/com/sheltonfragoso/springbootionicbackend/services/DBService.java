92a54690d47c7e599b7b631116a0afaaf33767ea
sheltonfragoso <shedvaldo1@gmail.com>
2022-02-21 21:02:49 +0200

    Salvando perfis de usuario na base de dados

--- src/main/java/com/sheltonfragoso/springbootionicbackend/services/DBService.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/services/DBService.java
@@ -20,6 +20,7 @@ import com.sheltonfragoso.springbootionicbackend.domain.PagamentoComCartao;
 import com.sheltonfragoso.springbootionicbackend.domain.Pedido;
 import com.sheltonfragoso.springbootionicbackend.domain.Produto;
 import com.sheltonfragoso.springbootionicbackend.domain.enums.EstadoPagamento;
+import com.sheltonfragoso.springbootionicbackend.domain.enums.Perfil;
 import com.sheltonfragoso.springbootionicbackend.domain.enums.TipoCliente;
 import com.sheltonfragoso.springbootionicbackend.repositories.CategoriaRepository;
 import com.sheltonfragoso.springbootionicbackend.repositories.CidadeRepository;
@@ -127,14 +128,20 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "jonasmatandofiniasse@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "jonasmatandofiniasse1@gmail.com", "32709324059", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
