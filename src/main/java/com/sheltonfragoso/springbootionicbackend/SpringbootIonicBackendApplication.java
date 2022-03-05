7ba3e31921f2d18ad41e81a6a1274f5e9c50ee7a
sheltonfragoso <shedvaldo1@gmail.com>
2022-03-05 15:30:14 +0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/sheltonfragoso/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/sheltonfragoso/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -1,11 +1,17 @@
 package com.sheltonfragoso.springbootionicbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sheltonfragoso.springbootionicbackend.services.S3Service;
+
 @SpringBootApplication
 public class SpringbootIonicBackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
@@ -13,6 +19,6 @@ public class SpringbootIonicBackendApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\img\\fotos\\java.png");
 	}
 }
